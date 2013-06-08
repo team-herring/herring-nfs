991ab61a8ad365f912b05986d24f65cdf7ef48b5
HyunjeJo <retriever89@gmail.com>
2013-06-09 01:52:56 +0900

    DirectoryServiceManagerDaemon 추가

---
+++ src/main/java/org/herring/nfs/DirectoryServiceManagerDaemon.java
@@ -0,0 +1,20 @@
+package org.herring.nfs;
+
+import org.herring.core.protocol.ServerComponent;
+
+/**
+ * << Description >>
+ * User: hyunje
+ * Date: 13. 6. 9.
+ * Time: 오전 12:56
+ */
+public class DirectoryServiceManagerDaemon {
+
+    private final static int port = 9300;
+
+    private ServerComponent serverComponent;
+
+    public static void main(String[] args){
+
+    }
+}
