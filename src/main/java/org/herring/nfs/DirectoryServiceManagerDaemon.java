773858782f2d660043511a370bdd7a6f4c874092
HyunjeJo <retriever89@gmail.com>
2013-06-10 23:46:16 +0900

    MessageHandler에 switch-case 추가

--- src/main/java/org/herring/nfs/DirectoryServiceManagerDaemon.java
+++ src/main/java/org/herring/nfs/DirectoryServiceManagerDaemon.java
@@ -1,5 +1,6 @@
 package org.herring.nfs;
 
+import org.herring.core.nfs.NetworkFileSystemAPIHandler;
 import org.herring.core.protocol.NetworkContext;
 import org.herring.core.protocol.ServerComponent;
 import org.herring.core.protocol.codec.HerringCodec;
@@ -24,15 +25,34 @@ public class DirectoryServiceManagerDaemon {
         HerringCodec codec = new SerializableCodec();
         MessageHandler messageHandler  =  new AsyncMessageHandler(){
             @Override
-        public boolean messageArrived(NetworkContext context, Object data) throws Exception{
-                context.sendObject("Message Arrived!");
-                return true;
+            public boolean messageArrived(NetworkContext context, Object data) throws Exception {
+                NetworkFileSystemAPIHandler apiHandler = (NetworkFileSystemAPIHandler) data;
+
+                switch (apiHandler.getCommand()){
+                    case putData_locate_data:
+                        break;
+                    case putData_locate_datalist:
+                        break;
+                    case getData_locate:
+                        break;
+                    case getData_locate_offset_size:
+                        break;
+                    case getLine_locate_linecount:
+                        break;
+                    default:
+                        System.out.println("Command Error");
+                }
+
+                context.sendObject("hello client!");
+                return true; // 비동기 통신이므로 현재 핸들러에서 메시지를 사용했음을 알린다.
             }
         };
-
         managerDaemon.serverComponent = new ServerComponent(port,codec,messageHandler);
         //Daemon 시작
         System.out.println("DirectoryServiceManager를 시작합니다.");
         managerDaemon.serverComponent.start();
+
+
+
     }
 }
