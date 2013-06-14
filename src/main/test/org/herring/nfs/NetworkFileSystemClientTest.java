d2594039d103e1a7f2ffebc79f60cedafd060e83
HyunjeJo <retriever89@gmail.com>
2013-06-14 16:54:08 +0900

    각 method 테스트 성공

--- src/main/test/org/herring/nfs/NetworkFileSystemClientTest.java
+++ src/main/test/org/herring/nfs/NetworkFileSystemClientTest.java
@@ -7,6 +7,9 @@ import org.junit.FixMethodOrder;
 import org.junit.Test;
 import org.junit.runners.MethodSorters;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * << Description >>
  * User: hyunje
@@ -32,36 +35,96 @@ public class NetworkFileSystemClientTest {
         client.closeConnection();
     }
 
+    /**
+     * boolean putData(String locate, String data) 테스트
+     * @throws Exception
+     */
     @Test
     public void test0PutData() throws Exception {
+        System.out.println("============================");
         System.out.println("test0PutData");
-        if (client.putData("101", "Test Data"))
+        if (client.putData("100", "Test Data"))
             System.out.println("Success");
         else
             System.out.println("Fail");
+        System.out.println("============================");
 
     }
 
+    /**
+     * boolean putData(String locate, List<String> data) 테스트
+     * @throws Exception
+     */
     @Test
     public void test1PutData() throws Exception {
+        System.out.println("============================");
         System.out.println("test1PutData");
+        String locate = "101";
+        List<String> dataList = new ArrayList<String>();
+        dataList.add("test data 1");
+        dataList.add("test data 2");
+        dataList.add("test data 3");
+        dataList.add("test data 4");
+        dataList.add("test data 5");
+        dataList.add("test data 6");
+        dataList.add("test data 7");
+        dataList.add("test data 8");
+
+        if (client.putData(locate, dataList))
+            System.out.println("Success");
+        else
+            System.out.println("Fail");
+        System.out.println("============================");
+
     }
 
+    /**
+     * boolean getData(String locate) 테스트
+     * @throws Exception
+     */
     @Test
     public void test2GetData() throws Exception {
-        System.out.println("test2GetData");
+        System.out.println("============================");
 
+        System.out.println("test2GetData");
+        String locate = "101";
+        String result = client.getData(locate);
+        System.out.println("In "+locate+" : "+result);
+        System.out.println("============================");
     }
 
+    /**
+     * boolean getData(String locate, int offset, int size) 테스트
+     * @throws Exception
+     */
     @Test
     public void test3GetData() throws Exception {
+        System.out.println("============================");
         System.out.println("test3GetData");
+        String locate = "101";
+        int offset = 8;
+        int size = 20;
+        String result = client.getData(locate,offset,size);
+        System.out.println("locate : "+locate);
+        System.out.println("offset : "+offset);
+        System.out.println("size : "+size);
+        System.out.println("result : "+result);
+        System.out.println("============================");
 
-    }
+}
 
+    /**
+     * boolean getLine(String locate, int linecount) 테스트
+     * @throws Exception
+     */
     @Test
     public void test4GetLine() throws Exception {
+        System.out.println("============================");
         System.out.println("test4GetLine");
-
+        String locate = "101";
+        int linecount = 5;
+        String result = client.getLine(locate,linecount);
+        System.out.println("result : "+result);
+        System.out.println("============================");
     }
 }
